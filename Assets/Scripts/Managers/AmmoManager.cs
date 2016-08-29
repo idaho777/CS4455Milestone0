fe7bd9a0e697bd5c1eec46a9a8e7b170a3e3daec
Joonho <idaho777@gmail.com>
2016-08-29 00:46:42 -0400

    Initial

---
+++ Assets/Scripts/Managers/AmmoManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AmmoManager : MonoBehaviour {
+    public static int ammo;
+    public static int maxAmmo;
+
+    Text text;
+    
+	void Start () {
+        text = GetComponent <Text> ();
+    }
+	
+	void Update ()
+    {
+        text.text = "Ammo: " + ammo + "/" + maxAmmo + " (Reload : r)";
+    }
+}
